13241d6e2a3bd5e24c7ef2ea6690c67811e6166b
Dave Woodall <dmwoodall@gmail.com>
2012-05-07 06:39:06 -0500

    built add method

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,26 +1,69 @@
 require 'restaurant'
 class Guide
 	
-	def initialize(path=nil)
-		# Locatate rest text file 
-		Restaurant.filepath = path
-		if Restaurant.file_exists?
-			puts "Found file"
-		elsif Restaurant.create_file
-			puts "Created file"
-		else
-			puts "Exiting. \n\n"
-			exit!
-		end
+	class Config
+		@@actions = [ 'list', 'find', 'add', 'quit']
+		def self.actions; @@actions; end
 	end
+	
+def initialize(path=nil)
+	# Locatate rest text file 
+	Restaurant.filepath = path
+	if Restaurant.file_usable?
+		puts "You have an existing restaurant file!"
+	elsif Restaurant.create_file
+		puts "We have created a new restaurant file!"
+	else
+		puts "Exiting. \n\n"
+		exit!
+	end
+end
 	
 	def launch!
 		introduction
-		# action loops
-		#  what do you want to do? - list, find, add, quit
-		#  do the action
-		# repeate until user quits
-		conclusion
+		result = nil
+		until result == :quit
+			action = get_action
+			result = do_action(action)
+		end
+		conclusion		
+	end
+	
+	def get_action
+		action = nil
+		until	Guide::Config.actions.include?(action) 
+			puts "Please choose one of the these actions: " + Guide::Config.actions.join(", ") if action
+			print "> "
+			user_response = gets.chomp
+			action = user_response.downcase.strip
+		end
+			return action
+	end
+	
+	
+	def do_action(action)
+		case action
+		when 'list'
+			puts 'listing...'
+		when 'find'
+			puts 'finding...'
+		when 'add'
+			add
+		when 'quit'
+			return :quit
+		else
+			puts "\n I don't understand that command. \n"
+		end
+	end
+	
+	def add
+		puts "\n ADD A RESTAURANT \n"
+		restaurant = Restaurant.build_using_questions
+		if restaurant.save
+			puts "\n Restaurant Added\n\n"
+		else
+			puts "\n Save Error: Restaurant not added. \n\n"
+		end
 	end
 	
 	def introduction
@@ -32,3 +75,21 @@ class Guide
 		puts "\n<<< Goodbye and Bon Appetite! >>> \n\n\n"
 	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -4,14 +4,60 @@ class Restaurant
 	def self.filepath= (path=nil)
 		@@filepath = File.join(APP_ROOT, path)
 	end
-
+	
+	attr_accessor :name, :cuisine, :price
+	
 	def self.file_exists?
+		if @@filepath && File.exists?(@@filepath)
+			return true
+		else
+			return false
+		end
+	end
+
+	def self.file_usable?
+		return false unless @@filepath
+		return false unless File.exists?(@@filepath)
+		return false unless File.readable?(@@filepath)
+		return false unless File.writable?(@@filepath)
+		return true
 	end
 
 	def self.create_file
+		File.open(@@filepath, 'w') unless file_exists?
+		return file_usable?
 	end
 
 	def self.saved_restaurants
+	end
+
+	def initialize(args={})
+		@name    = args[:name] || ""
+		@cuisine = args[:cuisine] || ""
+		@price   = args[:price] || ""
+	end
+
+	def self.build_using_questions
+		args = {}
+		
+		print "Restaurant name: "
+		args[:name] = gets.chomp.strip
+		
+		print "Cuisine type: "
+		args[:cuisine] = gets.chomp.strip
+		
+		print "Average price: "
+		args[:price] = gets.chomp.strip		
+
+		return self.new(args)
+	end
+
+	def save
+		return false unless Restaurant.file_usable?
+		File.open(@@filepath, 'a') do |file|
+			file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
+		end
+		return true
 	end
 	
 end
